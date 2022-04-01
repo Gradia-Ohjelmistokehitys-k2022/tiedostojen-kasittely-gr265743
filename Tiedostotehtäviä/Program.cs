f7602723b1fa25dd1b390e0e03cfddd315f5a404
Tommy Pamilo <AC2127@student.jamk.fi>
2022-04-01 15:14:51 +0300

    Git Add

---
+++ Tiedostotehtäviä/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace Tidostotehtäviä
+{
+    public class Tidostotehtäviä
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+
+            string tiedosto = @"C:\Users\tommy\tekstii.txt";
+            using (FileStream uusi = File.Create(tiedosto))
+
+
+                Console.WriteLine("Tarkistetaan : anna tiedoston nimi: ");
+            Console.ReadLine();
+
+            if (File.Exists(tiedosto))
+            {
+                Console.WriteLine("tiedosto on olemassa");
+            }
+            else
+            {
+                Console.WriteLine("Tiedosta ei löydy");
+            }
+            using (StreamWriter writer = new StreamWriter(tiedosto))
+            {
+                writer.WriteLine("Hello world");
+                writer.WriteLine("uusi rivi tässä");
+                
+            }
+             
+
+
+            Console.WriteLine("luetaan tiedosto taulukoksi");
+            Console.WriteLine(File.ReadAllText(@"C:\Users\tommy\tekstii.txt"));
+            Console.ReadLine();
+            
+            Console.WriteLine("Luetaan tiedoto merkkijonoksi");
+            Console.WriteLine((@"C:\Users\tommy\tekstii.txt"));
+            Console.ReadLine();
+           
+            Console.WriteLine("Luetaan tiedosto listaksi");
+            Console.WriteLine(File.ReadAllText(@"C:\Users\tommy\tekstii.txt"));
+            Console.ReadLine();
+
+
+            string readText = File.ReadAllText(tiedosto);
+            Console.WriteLine(readText);
+
+            char pilkku;
+            int pilkulla = 0;
+            StreamReader lukee;
+            lukee = new StreamReader(@"C:\Users\tommy\tekstii.txt");
+            do 
+            {
+                pilkku = (char)lukee.Read();
+                Console.Write(" , ");
+                 Console.Write(pilkku);
+
+
+                pilkulla++;
+            } while (!lukee.EndOfStream);
+            lukee.Close();
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+    
+
+
+
+
+
+        
---
+++ Tiedostotehtäviä/Tiedosto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tidostotehtäviä
+{
+    public class Tiedosto
+    {
+        public static bool tiedostoOnluotu(string tiedosto)
+        {
+            return File.Exists(tiedosto);
+        }
+
+        public static List<string> Luelistaksi(string tiedosto)
+        {
+            string[] rivit = Luetaulukoiksi(tiedosto);
+            List<string> taulukko = new List<string>();
+
+            return taulukko;
+        }
+
+        public static string luejonoksi(string tiedosto)
+        {
+            string[] taulukko = Luetaulukoiksi(tiedosto);
+            string returnn = "";
+            foreach (string rivit in taulukko)
+                returnn += taulukko;
+            return returnn;
+        }
+        public static string[] Luetaulukoiksi(string tiedosto)
+        {
+            string[] kaikki;
+            kaikki = File.ReadAllLines(tiedosto);
+            return kaikki;
+        }
+
+
+
+        public static List<string> luelistaksi(string tiedosto)
+        {
+            string[] kaikki = Luetaulukoiksi(tiedosto);
+            List<string> lista = new List<string>();
+
+            return lista;
+        }
+
+
+    }
+    }
+
